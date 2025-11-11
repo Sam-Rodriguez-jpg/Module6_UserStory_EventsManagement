58fb9743d6f8349392c9674a5929dfb9125016d6
Sam Rodríguez <alguien8191@gmail.com>
2025-11-11 07:55:23 -0500

    First Commit

---
+++ src/main/java/com/example/demo/Module6UserStory1ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module6UserStory1ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module6UserStory1ProjectApplication.class, args);
+	}
+
+}
