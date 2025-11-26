83a79c1c3ef93da9f1b3c86ba952bd613b47d898
Sam Rodríguez <alguien8191@gmail.com>
2025-11-26 08:15:34 -0500

    feat: US4 - Commit #2 - Query optimization with JPQL and EntityGraph

--- src/main/java/com/example/demo/infrastructure/adapters/out/jpa/repositories/EventJpaRepository.java
+++ src/main/java/com/example/demo/infrastructure/adapters/out/jpa/repositories/EventJpaRepository.java
@@ -1,7 +1,22 @@
 package com.example.demo.infrastructure.adapters.out.jpa.repositories;
 
 import com.example.demo.infrastructure.adapters.out.jpa.entites.EventEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface EventJpaRepository extends JpaRepository<EventEntity, Long>, JpaSpecificationExecutor<EventEntity> {}
+import java.util.List;
+
+public interface EventJpaRepository extends JpaRepository<EventEntity, Long>, JpaSpecificationExecutor<EventEntity> {
+
+    // Consulta avanzada con JPQL (Java Persistence Query Language)
+    @Query("SELECT events FROM EventEntity events JOIN FETCH events.venueEntity venues WHERE venues.idVenue = :idVenue")
+    List<EventEntity> findByVenueIdFetch(@Param("idVenue") Long idVenue);
+
+    // Elimination del N+1 con EntityGraph
+    @EntityGraph(attributePaths = {"venueEntity"})
+    @Query("SELECT events FROM EventEntity events")
+    List<EventEntity> findAllWithVenue();
+}
